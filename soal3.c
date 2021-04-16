7705729927957097a58b26aac28f4046c356d90d
christianbennett <christianbennettrobin@gmail.com>
2021-04-16 13:08:53 +0700

    chdir..

--- soal3.c
+++ soal3.c
@@ -66,17 +66,16 @@ int main(int argc, char *argv[])
         timeinfo = localtime(&rawtime);
         strftime(stringTime, sizeof(stringTime), "%Y-%m-%d_%X", timeinfo);
 
-        //soalA
         pidA = fork();
         if (pidA < 0)
         {
-            exit(EXIT_FAILURE);
+            exit(0);
         }
         if (pidA == 0)
         {
             //buat direktori 3a
-            char *argvA[] = {"mkdir", stringTime, NULL};
-            execv("/bin/mkdir", argvA);
+            char *argv[] = {"mkdir", stringTime, NULL};
+            execv("/bin/mkdir", argv);
         }
 
         //soalB
@@ -86,12 +85,12 @@ int main(int argc, char *argv[])
         }
         if (pidB < 0)
         {
-            exit(EXIT_FAILURE);
+            exit(0);
         }
         if (pidB == 0)
         {
             chdir(stringTime);
-            for (int i = 0; i < 10; i++)
+            for (i = 0; i < 10; i++)
             {
                 //mendapatkan waktu saat mendownload gambar
                 time_t rawtime2;
@@ -102,37 +101,25 @@ int main(int argc, char *argv[])
                 strftime(stringTime2, sizeof(stringTime2), "%Y-%m-%d_%X", timeinfo2);
                 //modifikasi string url agar bisa download file sesuai kriteria
                 sprintf(url, "https://picsum.photos/%ld", (rawtime2 % 1000) + 50);
-                //mendownload dengan size sesuai kriteria
+
                 pidC = fork();
                 if (pidC < 0)
-                    exit(EXIT_FAILURE);
+                {
+                    exit(0);
+                }
                 if (pidC == 0)
                 {
-                    //download gambar dari url
-                    char *argvB[] = {"wget", url, "-O", stringTime2, NULL};
-                    execv("/usr/bin/wget", argvB);
-                }
-                if (i == 9)
-                {
-                    printf("\n\nDONE\n\n");
-                    chdir("..");
+                    char *argv[] = {"wget", url, "-O", stringTime2, NULL};
+                    execv("/usr/bin/wget", argv);
                 }
                 sleep(5);
             }
-            // while (wait(&statusB) > 0)
-            // {
-            //     chdir("..");
-            // }
-        }
 
-        // pidD = fork();
-        // if (pidD < 0)
-        // {
-        //     exit(EXIT_FAILURE);
-        // }
-        // if (pidD == 0)
-        // {
-        // }
+            while (wait(&statusB) > 0)
+            {
+                chdir("..");
+            }
+        }
         sleep(40);
     }
 }
