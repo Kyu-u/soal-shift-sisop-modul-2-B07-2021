8ddb7847bcc3e5822952ef2d9aa751c1ba55c1cd
christianbennett <christianbennettrobin@gmail.com>
2021-04-16 13:57:44 +0700

    try cypher

--- soal3.c
+++ soal3.c
@@ -44,19 +44,29 @@ void daemonSkeleton()
     // close(STDERR_FILENO);
 }
 
+void caesar5(char word[])
+{
+    int len = strlen(word);
+    for (int i = 0; i < len; i++)
+    {
+        word[i] += 5;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int status, statusA, statusB, statusC;
+    int status;
 
     while (wait(&status) > 0)
-    {
-        daemonSkeleton();
-    }
+        ;
+
+    daemonSkeleton();
 
     while (1)
     {
         pid_t pidA, pidB, pidC, pidD, pidE;
-        char url[40];
+
+        int statusA;
 
         //untuk mendapatkan waktu saat program dieksekusi
         time_t rawtime;
@@ -69,7 +79,7 @@ int main(int argc, char *argv[])
         pidA = fork();
         if (pidA < 0)
         {
-            exit(0);
+            exit(EXIT_FAILURE);
         }
         if (pidA == 0)
         {
@@ -80,18 +90,23 @@ int main(int argc, char *argv[])
 
         //soalB
         while (wait(&statusA) > 0)
-        {
-            pidB = fork();
-        }
+            ;
+
+        pidB = fork();
+
         if (pidB < 0)
         {
-            exit(0);
+            exit(EXIT_FAILURE);
         }
         if (pidB == 0)
         {
+            int statusB;
+
             chdir(stringTime);
             for (int i = 0; i < 10; i++)
             {
+                char url[40];
+
                 //mendapatkan waktu saat mendownload gambar
                 time_t rawtime2;
                 struct tm *timeinfo2;
@@ -105,7 +120,7 @@ int main(int argc, char *argv[])
                 pidC = fork();
                 if (pidC < 0)
                 {
-                    exit(0);
+                    exit(EXIT_FAILURE);
                 }
                 if (pidC == 0)
                 {
@@ -116,9 +131,20 @@ int main(int argc, char *argv[])
             }
 
             while (wait(&statusB) > 0)
-            {
-                chdir("..");
-            }
+                ;
+
+            char status[] = {"Download Success"};
+            //caesar cypher 5
+            caesar5(status);
+
+            //masukkan kedalam file
+            FILE *ptr = NULL;
+            ptr = fopen("status.txt", "w");
+            fputs(status, ptr);
+            fclose(ptr);
+
+            //kembali ke direktori sebelumnya
+            chdir("..");
         }
         sleep(40);
     }
